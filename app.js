1f4dc6cb7101282f36250e2aa385fe7c5d84e6a9
KabokAkos <99403467+KabokAkos@users.noreply.github.com>
2025-10-07 08:44:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import postRoutes from "./routes/postsRoutes.js"
+import userRoutes from "./routes/usersRoutes.js"
+
+const PORT = 3000;
+
+const app = express();
+app.use(express.json())
+
+app.use("/posts", postRoutes)
+app.use("/users", userRoutes)
+app.listen(PORT, () =>{
+    console.log(`Server runs on ${PORT}`)
+})
